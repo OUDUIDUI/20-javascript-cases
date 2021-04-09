e84bef6e4ad026cb9ebf91ff4706a3e07ed2ffeb
oushihao <oushihao97@163.com>
2021-04-09 09:28:21 +0800

    guess_word

--- main.js
+++ main.js
@@ -1,25 +1,114 @@
 // 获取节点
-const toggle = document.getElementById('toggle');
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-const modal = document.getElementById('modal');
+const wordEl = document.getElementById("word");
+const wrongLettersEl = document.getElementById("wrong-letters");
+const playAgainBtn = document.getElementById("play-button");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
 
-// Toggle nav
-toggle.addEventListener('click',()=>
-    document.body.classList.toggle('show-nav')
-)
+const figureParts = document.querySelectorAll(".figure-part");
 
-// show modal
-open.addEventListener('click',()=> 
-    modal.classList.add('show-modal')
-)
+const words = ["application", "programming", "interface", "wonder"];
 
-// hide modal
-close.addEventListener('click',()=> 
-    modal.classList.remove('show-modal')
-)
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-// hide modal on outside click
-window.addEventListener('click',e=> {
-    e.target === modal ? modal.classList.remove('show-modal') : false;
-})
+const correctLetters = [];
+const wrongLetters = [];
+
+// 显示单词函数
+function displayWord() {
+  wordEl.innerHTML = `
+    ${selectedWord
+      .split("")
+      .map(
+        letter => `
+        <span class="letter">
+        ${correctLetters.includes(letter) ? letter : ""}
+        </span>
+        `
+      )
+      .join("")}
+    `;
+  const innerWord = wordEl.innerText.replace(/\n/g, "");
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "恭喜你输入正确！ 😃";
+    popup.style.display = "flex";
+  }
+}
+
+// updateWrongLettersEl
+function updateWrongLettersEl() {
+  // 显示错误字母
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? "<p>错误</p>" : ""}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+
+  // 显示火柴人身体
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = "block";
+    } else {
+      part.style.display = "none";
+    }
+  });
+
+  // 机会用完显示弹出框
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = "抱歉输入错误，游戏结束. 😕";
+    popup.style.display = "flex";
+  }
+}
+
+// showNotification函数
+function showNotification() {
+  notification.classList.add("show");
+
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 3000);
+}
+// 按下键盘中的字母的事件监听
+window.addEventListener("keydown", e => {
+  // console.log(e.keyCode);
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
+//再玩一次按钮的事件监听
+playAgainBtn.addEventListener("click", () => {
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = "none";
+});
+
+displayWord();
