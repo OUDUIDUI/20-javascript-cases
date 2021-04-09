cca0f024bfaa6e00a88d658a6f8393a36884c7b7
oushihao <oushihao97@163.com>
2021-04-09 09:26:42 +0800

    dom_array_methods

--- main.js
+++ main.js
@@ -1,38 +1,85 @@
 // 获取节点
-const currentEl_one = document.getElementById('currency-one');
-const amountEl_one = document.getElementById('amount-one');
-const currentEl_two = document.getElementById('currency-two');
-const amountEl_two = document.getElementById('amount-two');
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
 
-const swap = document.getElementById('swap');
-const rateEl = document.getElementById('rate');
+let data = [];
 
-// 获取汇率，并实现dom节点更新
-function calculate(){
-    const currency_one = currentEl_one.value;
-    const currency_two = currentEl_two.value;
-
-    fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
-        .then(res =>res.json())
-        .then(data =>{
-            const rate = data.rates[currency_two];
-            
-            rateEl.innerText = `1${currency_one} = ${rate}${currency_two}`;
-            amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
-        })
+// Fetch random user and money
+async function getRandomUser(){
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+    
+    const user = data.results[0];
+    const newUser = {
+        name:`${user.name.first} ${user.name.last}`,
+        money:Math.floor(Math.random()*1000000)
+    }
+    addData(newUser);
 }
 
-// 事件监听
-currentEl_one.addEventListener('change',calculate);
-amountEl_one.addEventListener('input',calculate);
-currentEl_two.addEventListener('change',calculate);
-amountEl_two.addEventListener('input',calculate);
+// double money
+function doubleMoney(){
+    data = data.map(user =>{
+        return ({...user,money:user.money * 2})
+    })
 
-swap.addEventListener('click',()=>{
-    const temp = currentEl_one.value;
-    currentEl_one.value = currentEl_two.value;
-    currentEl_two.value = temp;
-    calculate();
-})
+    updataDOM();
+}
 
-calculate()
+// sort By Richest
+function sortByRichest(){
+    data.sort((a,b) => b.money - a.money);
+    updataDOM();
+}
+
+// show Millionaires
+function showMillionaires(){
+    data = data.filter(user => user.money > 1000000);
+    updataDOM();
+}
+
+// calculate Wealth
+function calculateWealth(){
+    const wealth = data.reduce((acc,user)=>(acc += user.money),0);
+    
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth:<strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
+
+// 添加随机生成对象到data数组
+function addData(obj){
+    data.push(obj);
+    updataDOM();
+}
+
+function updataDOM(providedData = data){
+    // clear main div
+    main.innerHTML = `<h2><strong>Person</strong>Wealth</h2>`
+
+    providedData.forEach( item =>{
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong>${formatMoney(item.money)}`
+        
+        main.appendChild(element);
+    })
+}
+
+// 转换为货币格式
+function formatMoney(number){
+    return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
+
+
+// 时间监听
+addUserBtn.addEventListener('click',getRandomUser);
+doubleBtn.addEventListener('click',doubleMoney);
+sortBtn.addEventListener('click',sortByRichest);
+showMillionairesBtn.addEventListener('click',showMillionaires);
+calculateWealthBtn.addEventListener('click',calculateWealth);
