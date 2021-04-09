6eb536981de89e4ab817da17f1fc096b4cb83a66
oushihao <oushihao97@163.com>
2021-04-09 09:29:58 +0800

    expense_tracker

--- main.js
+++ main.js
@@ -1,118 +1,101 @@
-// 获得节点
-const search = document.getElementById("search");
-const submit = document.getElementById("submit");
-const random = document.getElementById("random");
-const mealsEl = document.getElementById("meals");
-const resultHeading = document.getElementById("result-heading");
-const single_mealEl = document.getElementById("single-meal");
+// 获取DOM节点
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
 
+// 获取localStorage数据
+const localStorageTransactions = localStorage.getItem('transactions');
+let transactions = localStorageTransactions ? JSON.parse(localStorageTransactions) : [];
 
-// 通过fetch API 获得食谱数据
-function searchMeal(e){
+// 表单提交
+function addTransaction(e) {
+    // 阻止表单默认事件
     e.preventDefault();
-
-    // 清空single meal
-    single_mealEl.innerHTML = "";
-
-    // 获取search输入框的值
-    const term = search.value;
-    
-    // 检查是否为空
-    if(term.trim()){
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-            .then(res => res.json())
-            .then(data =>{
-                console.log(data);
-                if(data.meals === null){
-                    resultHeading.innerHTML = `<p>No results found</p>`;
-                }else{
-                    resultHeading.innerHTML = `<h2>Search results for '${term}'</h2>`;
-                    mealsEl.innerHTML = data.meals.map(meal => `
-                        <div class="meal">
-                            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-                            <div class="meal-info" data-mealId="${meal.idMeal}">
-                                <h3>${meal.strMeal}</h3>
-                            </div>
-                        </div>
-                    `).join('')
-                }
-            });
-
-        // 清空搜索框
-        search.value = "";
+    // 表单校验
+    if(text.value.trim() === '' || amount.value.trim() === ''){
+        alert("请输入交易名称和金额")
     }else{
-        alert("请输入搜索的内容")
+        const transaction = {
+            id: generateID(),
+            text: text.value,
+            amount: +amount.value
+        };
+        transactions.push(transaction);
+        addTransactionDOM(transaction)
+        updateValues()
+        updateLocalStorage();
+
+        text.value = '';
+        amount.value = '';
     }
 }
 
-// 获取食谱
-function getMealById(mealID) {
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
-      .then(res => res.json())
-      .then(data => {
-        const meal = data.meals[0];
-        addMealToDOM(meal);
-      });
+// 创建generateID
+function generateID() {
+    return Math.floor(Math.random()*100000000)
 }
 
-// 获取随机食谱
-function getRandomMeal() {
-    mealsEl.innerHTML = "";
-    resultHeading.innerHTML = "";
-  
-    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
-      .then(res => res.json())
-      .then(data => {
-        const meal = data.meals[0];
-        addMealToDOM(meal);
-      });
-}
+// 添加transaction交易到Dom节点
+function addTransactionDOM(transaction){
+    // 判断收入还是支出
+    const sign = transaction.amount < 0 ? '-' : '+';
+    // 创建li标签
+    const item  = document.createElement('li');
+    // 基于金额正负添加对应的类名
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
-function addMealToDOM(meal) {
-    const ingredients = [];
-    for (let i = 1; i <= 20; i++) { 
-        if (meal[`strIngredient${i}`]) {
-            ingredients.push(
-                `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
-                );
-        } else {
-            break;
-        }
-    }
-
-    single_mealEl.innerHTML = `
-    <div class="single-meal">
-        <h1>${meal.strMeal}</h1>
-        <img src= "${meal.strMealThumb}" alt="${meal.strmeal}">
-        <div class="single-meal-info">
-            ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
-            ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
-        </div>
-        <div class="main">
-            <p>${meal.strInstructions}</p>
-            <h2>Ingredients</h2>
-            <ul>
-                ${ingredients.map(ing => `<li>${ing}</li>`).join("")}
-            </ul>
-        </div>
-    </div>
+    item.innerHTML = `
+     ${transaction.text}
+     <span>${sign}${Math.abs(transaction.amount)}</span>
+     <button class="delete-btn" onclick="removeTransaction(${transaction.id})">X</button>   
     `;
+    list.appendChild(item);
 }
+
+// 更新余额、收入、支出的金额
+function updateValues(){
+    // 通过map()获得交易金额数组
+    const amounts = transactions.map(transaction => transaction.amount);
+    // 余额
+    const total = amounts.reduce((acc,item) => (acc+item),0).toFixed(2);
+    // 收入
+    const income = amounts.filter(amount => amount > 0)
+        .reduce((acc,item) => (acc+item),0)
+        .toFixed(2);
+    // 支出
+    const expense = amounts.filter(amount => amount < 0)
+        .reduce((acc,item) => (acc+item),0)
+        .toFixed(2);
+    // 更新DOM
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+}
+
+// 删除记录
+function removeTransaction(id){
+    transactions = transactions.filter(transaction => transaction.id !== id);
+    updateLocalStorage();
+    init();
+}
+
+// 更新本地存储数据
+function updateLocalStorage() {
+    localStorage.setItem('transactions',JSON.stringify(transactions));
+}
+
+// 初始化应用
+function init(){
+    list.innerHTML = '';
+    transactions.forEach(transaction => addTransactionDOM(transaction));
+    updateValues()
+}
+
+init()
 
 // 事件监听
-submit.addEventListener("submit", searchMeal);
-random.addEventListener("click", getRandomMeal);
-
-mealsEl.addEventListener("click", e => {
-    const mealInfo = e.path.find(item => {
-        if (item.classList) {
-            return item.classList.contains("meal-info");
-        } else {
-            return false;
-        }
-    });
-    if (mealInfo) {
-        const mealID = mealInfo.getAttribute("data-mealid");
-        getMealById(mealID);
-    }
-});
+form.addEventListener('submit',addTransaction);
