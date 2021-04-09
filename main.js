360dc5cfd17f25d5c5e34e488a8f505f6e36cec8
oushihao <oushihao97@163.com>
2021-04-09 09:36:28 +0800

    speech_text_reader

--- main.js
+++ main.js
@@ -1,122 +1,140 @@
 // 获取节点
-const word = document.getElementById("word");
-const text = document.getElementById("text");
-const scoreEl = document.getElementById("score");
-const timeEl = document.getElementById("time");
-const endgameEl = document.getElementById("end-game-container");
-const settingsBtn = document.getElementById("settings-btn");
-const settings = document.getElementById("settings");
-const settingsForm = document.getElementById("settings-form");
-const difficultySelect = document.getElementById("difficulty");
+const main = document.querySelector("main");
+const voicesSelect = document.getElementById("voices");
+const textarea = document.getElementById("text");
+const readBtn = document.getElementById("read");
+const toggleBtn = document.getElementById("toggle");
+const closeBtn = document.getElementById("close");
 
-// 游戏单词
-const words = [
-    "sigh",
-    "tense",
-    "airplane",
-    "ball",
-    "pies",
-    "juice",
-    "warlike",
-    "bad",
-    "north",
-    "dependent",
-    "steer",
-    "silver",
-    "highfalutin",
-    "superficial",
-    "quince",
-    "eight",
-    "feeble",
-    "admit",
-    "drag",
-    "loving"
+// 创建data
+const data = [
+    {
+        image: "./img/spring01.jpg",
+        text: "春暖花开"
+    },
+    {
+        image: "./img/summer01.jpg",
+        text: "夏阳酷暑"
+    },
+    {
+        image: "./img/autumn01.jpg",
+        text: "雁过留声"
+    },
+    {
+        image: "./img/winter01.jpg",
+        text: "白雪皑皑"
+    },
+    {
+        image: "./img/spring02.jpg",
+        text: "草长莺飞"
+    },
+    {
+        image: "./img/summer02.jpg",
+        text: "骄阳似火"
+    },
+    {
+        image: "./img/autumn02.jpg",
+        text: "一叶知秋 "
+    },
+    {
+        image: "./img/winter02.jpg",
+        text: "瑞雪纷飞"
+    },
+    {
+        image: "./img/spring03.jpg",
+        text: "鸟语花香"
+    },
+    {
+        image: "./img/summer03.jpg",
+        text: "艳阳高照"
+    },
+    {
+        image: "./img/autumn03.jpg",
+        text: "秋风瑟瑟"
+    },
+    {
+        image: "./img/winter03.jpg",
+        text: "雪中送炭"
+    }
 ];
 
-// 初始单词（随机）
-let randomWord;
+data.forEach(createBox);
 
-// 初始得分
-let score = 0;
-
-// 初始时间
-let time = 10;
-
-// 难度选择
-let difficulty = localStorage.getItem('difficulty') || 'easy';
-
-// 更新难度选项
-difficultySelect.value = difficulty;
-
-// 聚焦到input输入框
-text.focus();
-
-// 倒计时
-const timeInterval = setInterval(updateTime,1000);
-
-// 设置随机产生单词
-function getRandomWord() {
-    return words[Math.floor(Math.random() * words.length)];
-}
-
-// 更新单词到DOM
-function addWordToDOM(){
-    randomWord = getRandomWord();
-    word.innerHTML = randomWord;
-}
-
-// 更新得分
-function updateScore(){
-    score++;
-    scoreEl.innerHTML = score;
-}
-
-// 更新剩余时间
-function updateTime(){
-    time--;
-    timeEl.innerHTML = `${time}s`
-
-    if(time === 0){
-        // 游戏结束
-        clearInterval(timeInterval);
-        gameOver();
-    }
-}
-
-// 提示游戏结束
-function gameOver(){
-    endgameEl.innerHTML = `
-        <h1>游戏结束</h1>
-        <p>您的最终得分${score}</p>
-        <button onclick="location.reload()">再玩一次</button>
+// 创建createBox
+function createBox(item) {
+    const box = document.createElement('div');
+    const {image,text} = item;
+    box.classList.add('box')
+    box.innerHTML = `
+        <img src="${image}" alt="${text}" >
+        <p class="info">${text}</p>
     `;
-    endgameEl.style.display = 'flex';
+    // 点击按钮进行阅读
+    box.addEventListener('click',()=>{
+        box.classList.add('active');
+
+        setTextMessage(text);
+        speakText();
+
+        setTimeout(()=>{
+            box.classList.remove('active');
+        },800)
+    })
+
+    main.appendChild(box);
 }
 
-addWordToDOM();
+// 初始化speedsynth
+const message = new SpeechSynthesisUtterance();
 
-// 事件监听
-text.addEventListener('input',e =>{
-    const insertedText = e.target.value;
+// 语音变量
+let voices = [];
+function getVoices(){
+    voices = speechSynthesis.getVoices();
 
-    // 单词匹配
-    if(insertedText === randomWord){
-        addWordToDOM();
-        updateScore();
-        e.target.value = '';
+    voices.forEach(voice =>{
+        const option = document.createElement('option');
+        option.value = voice.name;
+        option.innerText = `${voice.name} ${voice.lang}`;
+        voicesSelect.appendChild(option);
+    })
+}
 
-        time +=
-            difficulty === 'hard' ? 2 :
-                (difficulty === 'hard' ? 3 : 5);
-        updateTime();
-    }
+// 获得文字
+function setTextMessage(text){
+    message.text = text;
+}
+
+// 阅读文字
+function speakText(){
+    speechSynthesis.speak(message);
+}
+
+// 切换语音
+function setVoice(e){
+    message.voice = voices.find(voice => voice.name === e.target.value);
+}
+
+// 切换语音事件监听
+speechSynthesis.addEventListener('voiceschanged',getVoices);
+
+// 切换文字框的事件监听
+toggleBtn.addEventListener('click',()=>{
+    document.getElementById('text-box').classList.toggle('show');
 })
 
-settingsBtn.addEventListener('click',()=>{
-    settings.classList.toggle("hide");
+// 关闭按钮的事件监听
+closeBtn.addEventListener('click',()=>{
+    document.getElementById('text-box').classList.remove('show');
 })
 
-settingsForm.addEventListener('change', e =>{
-    difficulty = e.target.value;
-    localStorage.setItem('difficulty',difficulty)
+// 下拉框事件监听
+voicesSelect.addEventListener('change',setVoice);
+
+// 阅读文字按钮的事件监听
+readBtn.addEventListener('click',()=>{
+    setTextMessage(textarea.value);
+    speakText();
 })
+
+getVoices();
